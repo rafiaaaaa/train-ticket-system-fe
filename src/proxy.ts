5f97802b6d6c20bfc92cf1ab2b4941a976704374
rafia <akmalrafi1132@gmail.com>
2026-01-29 09:58:57 +0700

    fix: middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,29 +5,45 @@ const AUTH_COOKIE = "access_token";
 const PUBLIC_ROUTES = ["/auth"];
 const UNPROTECTED_ROUTES = ["/search", "/about", "/contact"];
 
-export function proxy(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = req.cookies.get(AUTH_COOKIE)?.value;
 
   const isPublicRoute = PUBLIC_ROUTES.some((route) =>
-    pathname.startsWith(route)
+    pathname.startsWith(route),
   );
 
   const isUnprotected = UNPROTECTED_ROUTES.some((route) =>
-    pathname.startsWith(route)
+    pathname.startsWith(route),
   );
 
   if (token && isPublicRoute) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  const authRoutes = !isPublicRoute && !isUnprotected && pathname !== "/";
-
-  if (authRoutes && !token) {
-    return NextResponse.redirect(new URL("/auth", req.url));
+  if (pathname === "/") {
+    return NextResponse.next();
   }
 
-  return NextResponse.next();
+  try {
+    const backendUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/me`;
+    const cookieHeader = req.headers.get("cookie") ?? "";
+
+    const res = await fetch(backendUrl, {
+      method: "GET",
+      headers: {
+        cookie: cookieHeader,
+      },
+    });
+
+    if (res.ok) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
+  } catch (err) {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
 }
 
 export const config = {
